239c241927f5a6592ee3dabecfe6dd6002cd52bf
AoEiuV020 <aoeiuv020@gmail.com>
2021-06-25 22:17:57 +0800

    change cache download path

--- script/download_ccache.sh
+++ script/download_ccache.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-git clone https://AoEiuV020:$PUSH_TOKEN@github.com/AoEiuV020/chromium-ccache-arm64.git $HOME/cache
+git clone https://AoEiuV020:$PUSH_TOKEN@github.com/AoEiuV020/chromium-ccache-arm64.git $ROOT/cache
