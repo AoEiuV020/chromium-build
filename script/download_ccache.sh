5bb1a73e735a6ebb329b0d91405d9ac540cd6d25
AoEiuV020 <aoeiuv020@gmail.com>
2021-06-25 23:01:32 +0800

    download cache depth 1

--- script/download_ccache.sh
+++ script/download_ccache.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-git clone https://AoEiuV020:$PUSH_TOKEN@github.com/AoEiuV020/chromium-ccache-arm64.git $ROOT/cache
+git clone --depth 1 https://AoEiuV020:$PUSH_TOKEN@github.com/AoEiuV020/chromium-ccache-arm64.git $ROOT/cache
