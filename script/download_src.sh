af22bc21df13237c40d86aaa9a13b781a079424f
AoEiuV020 <aoeiuv020@gmail.com>
2021-06-25 11:26:51 +0800

    调整源码下载
    只下载一个版本，

--- script/download_src.sh
+++ script/download_src.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 cd "$ROOT"
-fetch --nohooks android
+git clone --depth 1 "https://github.com/chromium/chromium.git" src
+gclient config --name=src --unmanaged "https://github.com/chromium/chromium.git"
+echo 'target_os = ["android"]' >> .gclient
+gclient sync --nohooks
 cd src
 ./build/install-build-deps-android.sh
 gclient runhooks
